3867018d7edda259b1fa4c0a58af109265698187
Kai Lyons <62919224+KaiLikesLinux@users.noreply.github.com>
2020-04-26 23:12:46 -0600

    Update installrc.sh

--- installrc.sh
+++ installrc.sh
@@ -1,10 +1,16 @@
 # KQuickRC Installation Script
+
+# Move this:
 mv ./quickrc.sh ~/.quickrc.sh
 echo "sh ~/.quickrc.sh" >> ~/.bashrc
 
 # Setup Root Base
-if [ $# -lt 0]; then
-  # Script Stuff Here
+if [ $# -lt 0 ]; then
+  if [ "$1" = "-rb" ||"$1" = "--root-base"]; then
+    # More Scrpit
+  else
+    # More Script
+  fi
 else
   # Script Stuff Here
 fi
