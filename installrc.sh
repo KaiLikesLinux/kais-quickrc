4231ef00183dff5bf24fe8f0f3b842ba61a461ac
Kai Lyons <62919224+KaiLikesLinux@users.noreply.github.com>
2020-04-26 23:33:16 -0600

    Finalize QuickRC Install Script

--- installrc.sh
+++ installrc.sh
@@ -1,20 +1,31 @@
 # KQuickRC Installation Script
+# Setup Functions
+root-base () {
+  [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+  mkdir /.quickrc-config
+  mv ./quickrc.sh /.quickrc-config/quickrc.sh
+  echo "sh /.quickrc-config/quickrc.sh" >> ~/.bashrc
+}
+
+home-base () {
+  mkdir ~/.quickrc-config
+  mv ./quickrc.sh ~/.quickrc.sh
+  echo "sh ~/.quickrc.sh" >> ~/.bashrc
+}
+
 # Setup Root Base
 if [ $# -gt 0 ]; then
   if [ "$1" = "-rb" || "$1" = "--root-base"]; then
-    [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
-    # More Scrpit
+    root-base
   elif [ "$1" = "-hb" || "$1" = "--home-base"]; then 
-    # More Script
+    home-base
   fi
 else
   echo "Would you like to add a root base? [Y/n]"
   read -p ": " answer
   if [ "$answer" = "y" || "$answer" = "Y" ]; then
-    # More Script
+    root-base
   else
-    echo "Running without root (Home base setup)"
-    mv ./quickrc.sh ~/.quickrc.sh
-    echo "sh ~/.quickrc.sh" >> ~/.bashrc
+    home-base
   fi
 fi
